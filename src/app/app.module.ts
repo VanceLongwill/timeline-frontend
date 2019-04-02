51a8bfc40ba1927760bf49224e885db5f0592eae
Vance Longwill <vancelongwill@gmail.com>
2019-04-02 18:36:27 +0100

    generate message & timeline components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageComponent } from './message/message.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
