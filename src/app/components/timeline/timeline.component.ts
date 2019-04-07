0ba69b3f3c2810f6d55835cab04a53a63e3ae59c
Vance Longwill <vancelongwill@gmail.com>
2019-04-07 19:40:48 +0300

    feat: add refresh timeline button

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -15,28 +15,18 @@ export class TimelineComponent implements OnInit {
   isLoadingMessages: Observable<boolean>;
   hasError: Observable<boolean>;
   messages: Observable<Message[]>;
-  // messages: Message[] = [
-  //   {
-  //     id: 1,
-  //     body: 'first message',
-  //     author: 'John Smith',
-  //     createdAt: new Date(),
-  //   },
-  //   {
-  //     id: 2,
-  //     body: 'second message',
-  //     author: 'Bob Smith',
-  //     createdAt: new Date(),
-  //   }
-  // ];
   constructor(private store: Store<State>) {
-    // store.messages.messages
+    // store.messages
     this.messages = store.pipe(select('messages'), select('messages'));
     this.isLoadingMessages = store.pipe(select('messages'), select('loading'));
     this.hasError = store.pipe(select('messages'), select('hasError'));
   }
 
   ngOnInit() {
+    this.store.dispatch(new MessagesFetch());
+  }
+
+  handleRefresh() {
     this.store.dispatch(new MessagesFetch());
   }
 
